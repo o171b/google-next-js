6d78d5b3fda0b1cf6454ed98d7580a8c0d430698
ainawi <coder71717@gmail.com>
2025-10-28 22:13:01 +0400

    add pagination component

--- src/components/WebSearchResults.jsx
+++ src/components/WebSearchResults.jsx
@@ -4,7 +4,7 @@ import PaginationButtons from './PaginationButtons';
 
 export default function WebSearchResults({ results }) {
   return (
-    <div className='w-full mx-auto px-3 sm:pb-24 pb-40 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
+    <div className='w-full mx-auto px-3 sm:pb-9 pb-9 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
       <p className='text-gray-600 text-sm mb-5 mt-3'>
         About {results.searchInformation?.formattedTotalResults} results (
         {results.searchInformation?.formattedSearchTime} seconds)
